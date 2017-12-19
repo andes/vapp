45390c8325c92a86a34e3fbfd1e712a19e6b94ec
Mariano Botta <marianoabotta@gmail.com>
2017-12-19 14:43:25 -0300

    HTTPS

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 var path = require("path");
 var express = require("express");
 var initialize_1 = require("../api/initialize");
+var websockets_1 = require("../api/websockets");
+var KEY_PEM = process.env.KEY_PEM || 'key.pem';
+var SERVER_CERT = process.env.SERVER_CERT || 'server.crt';
+var useSSL = (fs.existsSync(KEY_PEM) && fs.existsSync(SERVER_CERT));
 // Inicializa Express
 var app = express();
 // Inicializa la APP
@@ -25,25 +29,29 @@ app.all('*', function (req, res) {
 });
 // Inicia el servidor
 var http = require('http');
-http.createServer(app).listen(80, function () {
+var server = http.createServer(app);
+server.listen(80, function () {
     console.log('Inicio de ANDES en el puerto 80');
 });
+if (!useSSL) {
+    websockets_1.Websockets.initialize(server);
+}
 /**
  * Ejemplos para crear certificados
  * openssl req -newkey rsa:2048 -new -nodes -keyout key.pem -out csr.pem
  * openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out server.crt
  */
 // Si existen los certificados levantamos el server con HTTPS
-var KEY_PEM = process.env.KEY_PEM || 'key.pem';
-var SERVER_CERT = process.env.SERVER_CERT || 'server.crt';
-if (fs.existsSync(KEY_PEM) && fs.existsSync(SERVER_CERT)) {
+if (useSSL) {
     var https = require('https');
-    https.createServer({
+    var httpsServer = https.createServer({
         key: fs.readFileSync(KEY_PEM),
         cert: fs.readFileSync(SERVER_CERT)
-    }, app).listen(443, function () {
+    }, app);
+    httpsServer.listen(443, function () {
         console.log('Inicio de ANDES en el puerto 443');
     });
+    websockets_1.Websockets.initialize(httpsServer);
 }
 module.exports = app;
 //# sourceMappingURL=index.js.map
